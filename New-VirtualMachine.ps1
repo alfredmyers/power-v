a0992976129eb800bc3abfd2423d0817ccfea4f0
Alfred Myers <git@alfredmyers.com>
2019-09-06 19:29:35 -0300

    Removed hardcoded switch name
    Instead will get THE external switch whatever its name happens to be.
    I hope the script breaks if there's more than one external switch as
    that is not being accounted for.
    That's left as an exercise for future me.

--- New-VirtualMachine.ps1
+++ New-VirtualMachine.ps1
@@ -5,6 +5,7 @@ Param(
 	$IsoPath,
 	$SecureBootTemplate
 )
+$SwitchName = (Get-VMSwitch | Where-Object SwitchType -eq 'External').Name
 $VHDPath = Join-Path -Path (Get-VMHost).VirtualHardDiskPath -ChildPath ($Name + ".vhdx")
 if ($SecureBootTemplate -eq "MicrosoftWindows") {
 	$null = New-VHD -Path $VHDPath -SizeBytes $VHDSizeBytes 
@@ -12,7 +13,7 @@ if ($SecureBootTemplate -eq "MicrosoftWindows") {
 else {
 	$null = New-VHD -Path $VHDPath -SizeBytes $VHDSizeBytes -BlockSizeBytes 1MB
 }
-$VM = New-VM -Name $Name -MemoryStartupBytes $MemoryBytes -Generation 2 -BootDevice VHD -SwitchName External -VHDPath $VHDPath
+$VM = New-VM -Name $Name -MemoryStartupBytes $MemoryBytes -Generation 2 -BootDevice VHD -SwitchName $SwitchName -VHDPath $VHDPath
 Set-VM $VM -ProcessorCount 2 -MemoryMaximumBytes $MemoryBytes -AutomaticCheckpointsEnabled $false
 Add-VMDvdDrive $VM -Path $IsoPath
 Set-VMFirmware $VM -BootOrder ((Get-VMFirmware $VM).BootOrder | ? BootType -eq 'Drive') 
