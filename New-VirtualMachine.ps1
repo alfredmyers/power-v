1c660bc728fc6c49dad4b64f3b5ba7e5840b7e7f
Alfred Myers <git@alfredmyers.com>
2019-09-08 17:19:10 -0300

    Parameterized swtich name
    Added a parameter to receive an optional switch name.
    If no switch name is provided, will try to find a single switch with
    external connectivity.
    If the number of switches with that characteric is other than one,
    then no switch will be chosen and the behaviour is undefined.

--- New-VirtualMachine.ps1
+++ New-VirtualMachine.ps1
@@ -3,9 +3,21 @@ Param(
 	$MemoryBytes,
 	$VHDSizeBytes,
 	$IsoPath,
-	$SecureBootTemplate
+	$SecureBootTemplate,
+	$SwitchName
 )
-$SwitchName = (Get-VMSwitch | Where-Object SwitchType -eq 'External').Name
+
+# If a switch name hasn't been provided, it'll try to find a default.
+if (!$SwitchName) {
+	$Switches = Get-VMSwitch | Where-Object SwitchType -eq 'External'
+	
+	#If there's only one switch with external connectivity, that's it.
+	#Else, a switch name should have been provided.
+	if ($Switches.Count -eq 1) {
+		$SwitchName = $Switches.Name
+	}
+}
+
 $VHDPath = Join-Path -Path (Get-VMHost).VirtualHardDiskPath -ChildPath ($Name + ".vhdx")
 if ($SecureBootTemplate -eq "MicrosoftWindows") {
 	$null = New-VHD -Path $VHDPath -SizeBytes $VHDSizeBytes 
